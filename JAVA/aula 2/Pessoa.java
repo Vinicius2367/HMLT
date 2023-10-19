4b5fb9268e062b0ab8afcfde47fc536504b80944
Vinicius2367 <viniciusgabriel2367@gmail.com>
2023-10-18 21:11:58 -0300

    859c940

---
+++ JAVA/aula 2/Pessoa.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Pessoa {
+    public static void main(String[] args) {
+        
+    }
+}
--- php/aula 2/index.php
+++ php/aula 2/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,11 +15,12 @@
 
         <label for="">sobrenome </label>
         <input type="text" id="sobrenome" name="sobrenome"><br><br>
-
         <input type="submit" value="enviar">
+        
     </form>
 
-</body>
 
+
+</body>
 
 </html>
